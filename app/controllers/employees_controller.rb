9b57f8e2e2bc36836d40396a8b49f1f17f08d11f
Adam Tootle <adamtootle@gmail.com>
2013-04-11 00:22:37 -0400

    Adding user registration.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,29 @@
 class EmployeesController < ApplicationController
   def register
+    registration_email = "register@whoiscomingtoworktoday.com"
+    if params.has_key?(:to) && params.has_key?(:from) && params.has_key?(:subject) && params.has_key?(:text) && params[:to] == registration_email
 
+      company_name_lower = params[:subject].downcase
+      company = Company.where(:name => company_name_lower).first
+
+      if(company.nil?)
+        company = Company.new
+        company.name = company_name_lower
+        company.save
+      end
+
+      employee = Employee.where(:email_address => params[:from]).first
+
+      if(employee.nil?)
+        employee = Employee.new
+      end
+
+      employee.name = params[:text]
+      employee.email_address = params[:from]
+      employee.company_id = company.id
+      employee.save
+
+    end
   end
 
   def list
